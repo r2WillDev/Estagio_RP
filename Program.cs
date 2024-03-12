bc011531d8b59011b057825385072569fe3a8392
Arthur Willyams <arthurwillyams13@gmail.com>
2024-03-12 17:19:15 -0300

    primeiro commmit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿static bool Fibonacci(int num){
+    int a = 0, b = 1;
+    
+    while(b < num){
+        int tempo = a;
+        a = b;
+        b = tempo + b;
+    }
+    
+    return b == num;
+}
+
+Console.WriteLine("Informe um numero para verificação");
+int num = int.Parse(Console.ReadLine());
+
+if (Fibonacci(num)){
+    Console.WriteLine($"o numero {num} pertence a sequencia de fibonacci");
+} else {
+    Console.WriteLine($"O numero {num} não pertence a sequencia de fibonacci");
+}
+
